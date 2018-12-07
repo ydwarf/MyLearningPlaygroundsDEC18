738a0ed286401df225c758e4b3b2754c8bdbf1be
Olivier Béghain <olivier.beghain@icloud.com>
2018-12-07 22:47:40 +0100

    Adding the switch statement

--- MyFirstCheatingPlayground.playground/Contents.swift
+++ MyFirstCheatingPlayground.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
+
 var str = "Hello, playground"
+
+
+var timeYouWakeUp = 6
+
+switch timeYouWakeUp {
+    case 6:
+        print("Cook yourself a big breakfast")
+    
+    default:
+        print("Go out for a breakfast")
+}
